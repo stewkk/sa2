b936d4173cdd2f7cf15642b2573bd88f9574777c
Alexandr Starovoytov <saniy200311@gmail.com>
2020-11-04 12:39:53 +0300

    add info dialog

--- src/event_handlers.cpp
+++ src/event_handlers.cpp
@@ -1,8 +1,14 @@
 
 #include "MyFrameMain.h"
+#include <wx/aboutdlg.h>
 
 void MyFrameMain::wxMenuItem_info_menu_selection(wxCommandEvent& event) {
-    std::cout << "event handled" << std::endl;
+    wxAboutDialogInfo info;
+	info.SetName("sa2");
+	info.SetVersion("0.1");
+	info.SetDescription("Program to download subtitles from smotret-anime.online");
+	info.SetCopyright(wxT("(C) 2020 Alexandr Starovoytov <stewk6@gmail.com>"));
+	wxAboutBox(info);
     event.Skip();
 }
 
